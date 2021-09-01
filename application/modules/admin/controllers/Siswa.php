1dc7fce8b66ba7e4e86c2d8848e6a9ec3096ff2a
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2021-09-01 19:45:10 +0700

    sfsg

--- application/modules/admin/controllers/Siswa.php
+++ application/modules/admin/controllers/Siswa.php
@@ -187,8 +187,8 @@ class Siswa extends MX_Controller
 				//delete file from server
 				unlink(FCPATH . 'upload/excel/' . $upload_data['file_name']);
 
-				// echo json_encode($data);
-				// die;
+				echo json_encode($data);
+				die;
 				if (count($data) != 0) {
 					//$insert = $this->db->insert_batch('mahasiswa', $data);
 					$insert = $this->universal->insert_batch($data, 'siswa');
